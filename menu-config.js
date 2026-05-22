1b0144be5bdc03f2d98391aca1c00e9ac4214111
Matthierry <Clarky-BWFC@hotmail.co.uk>
2026-05-22 10:41:05 +0100

    Centralize burger menu links with shared config

---
+++ menu-config.js
@@ -0,0 +1,11 @@
+window.MCP_NAV_ITEMS = [
+  { label: 'Home', href: '/' },
+  { label: 'Match Result - Best Value', href: '/hda-value' },
+  { label: 'Match Result - Highest Probability', href: '/hda-highchance' },
+  { label: 'U/O 2.5 - Best Value', href: '/uo-value' },
+  { label: 'U/O 2.5 - Highest Probability', href: '/uo-highchance' },
+  { label: 'Historical Data', href: '/historical' },
+  { label: '2025/26 Season Review', href: '/season-review-2025-26' },
+  { label: 'World Cup 2026 Game', href: '/wc26/' },
+  { label: 'FAQs', href: '/faqs' }
+];
